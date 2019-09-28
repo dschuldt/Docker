d098102c4b8820f11288b210b0f3b62aacbd9d03
dschuldt <contact@schuldt.dev>
2019-09-28 15:54:08 +0200

    improved k3s setup

--- k3s/install_ingress_metallb.sh
+++ k3s/install_ingress_metallb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 printf "1. install ingress-nginx\n\n"
-kubectl apply -f ingress-nginx/mandatory.yml
-kubectl apply -f ingress-nginx/service-nodeport.yml
+kubectl apply -f ingress-nginx/01-mandatory.yml
+kubectl apply -f ingress-nginx/02-services.yml
 
 printf "\n\n2. install metallb\n\n"
-kubectl apply -f metallb/metallb.yml
-kubectl apply -f metallb/configuration.yml
+kubectl apply -f metallb/01-metallb.yml
+kubectl apply -f metallb/02-configuration.yml
 
 exit 0
--- k3s/install_linkerd.sh
+++ k3s/install_linkerd.sh
@@ -5,5 +5,6 @@ linkerd check --pre
 
 printf "\n\n2. install linkerd\n\n"
 linkerd install | kubectl apply -f -
+kubectl apply -f linkerd
 
 exit 0
